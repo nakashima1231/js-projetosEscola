85bc9706f9c4af71d82db4b1791c2b92d8f3d854
nakashima1231 <gabrielmnakashima2@gmail.com>
2024-05-15 11:36:04 -0300

    adicionando arquivos JS e CSS

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("purple");
+  
+    //console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 30, 150);
+    }
+  }
+  
